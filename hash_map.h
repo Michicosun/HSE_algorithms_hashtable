b5056e9a0c791e777963c79245f4726126d5d0b3
Mikhail Artemenko <michicosun@ya.ru>
2021-02-17 18:00:53 +0300

    fixed HashMap

--- hash_map.h
+++ hash_map.h
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <functional>
 #include <vector>
 
@@ -7,7 +8,7 @@ private:
     const static inline size_t min_cnt_rows = 10;
     const static inline size_t scale = 4;
     
-    typedef std::pair<KeyType, ValueType> node;
+    typedef std::unique_ptr<std::pair<const KeyType, ValueType>> node;
     
     size_t cur_capacity;
     size_t cur_size;
@@ -18,14 +19,18 @@ private:
     
     void rebuild() {
         if (cur_capacity < cur_size * scale && cur_size * scale < cur_size * scale) return;
-        cur_capacity = max(min_cnt_rows, 2 * cur_size);
+        cur_capacity = std::max(min_cnt_rows, 2 * cur_size);
         std::vector<std::vector<node>> next_table(cur_capacity);
-        for (auto it = begin(); it != end(); ++it) {
-            size_t ind = hasher(it->first) % cur_capacity;
-            next_table[ind].push_back(*it);
+        for (size_t i = 0; i < table.size(); ++i) {
+            for (auto &ptr : table[i]) {
+                size_t ind = hasher(ptr->first) % cur_capacity;
+                next_table[ind].push_back(std::move(ptr));
+            }
         }
-        swap(table, next_table);
+        table.swap(next_table);
     }
+    
+public:
     
     class iterator {
     private:
@@ -35,7 +40,6 @@ private:
         size_t ind;
         
         void find_next_occupied() {
-            ++ind;
             while (row < hash_table->cur_capacity && ind == hash_table->table[row].size()) {
                 ++row;
                 ind = 0;
@@ -44,29 +48,34 @@ private:
         
     public:
         
+        iterator () {}
+        
         iterator (HashMap* hash_map, size_t cell_row = 0, size_t cell_ind = 0) {
             hash_table = hash_map;
             row = cell_row;
             ind = cell_ind;
+            find_next_occupied();
         }
         
         iterator operator++ () {
+            ++ind;
             find_next_occupied();
             return *this;
         }
         
         iterator operator++ (int) {
             iterator oldIter(hash_table, row, ind);
+            ++ind;
             find_next_occupied();
             return oldIter;
         }
         
-        node& operator* () {
+        std::pair<const KeyType, ValueType>& operator* () {
             return *hash_table->table[row][ind];
         }
         
-        node* operator-> () {
-            return &hash_table->table[row][ind];
+        std::pair<const KeyType, ValueType>* operator-> () {
+            return &*(hash_table->table[row][ind]);
         }
         
         bool operator== (iterator other) {
@@ -85,8 +94,7 @@ private:
         size_t row;
         size_t ind;
         
-        void find_next_occupied() const {
-            ++ind;
+        void find_next_occupied() {
             while (row < hash_table->cur_capacity && ind == hash_table->table[row].size()) {
                 ++row;
                 ind = 0;
@@ -95,41 +103,44 @@ private:
         
     public:
         
+        const_iterator () {}
+        
         const_iterator (const HashMap* hash_map, size_t cell_row = 0, size_t cell_ind = 0) {
             hash_table = hash_map;
             row = cell_row;
             ind = cell_ind;
+            find_next_occupied();
         }
         
-        iterator operator++ () {
+        const_iterator operator++ () {
+            ++ind;
             find_next_occupied();
             return *this;
         }
         
-        iterator operator++ (int) {
-            iterator oldIter(hash_table, row, ind);
+        const_iterator operator++ (int) {
+            const_iterator oldIter(hash_table, row, ind);
+            ++ind;
             find_next_occupied();
             return oldIter;
         }
         
-        const node& operator* () const {
+        const std::pair<const KeyType, ValueType>& operator* () const {
             return *hash_table->table[row][ind];
         }
         
-        const node* operator-> () const {
-            return &hash_table->table[row][ind];
+        const std::pair<const KeyType, ValueType>* operator-> () const {
+            return &*(hash_table->table[row][ind]);
         }
         
-        bool operator== (iterator other) const {
+        bool operator== (const_iterator other) const {
             return other.hash_table == hash_table && other.row == row && other.ind == ind;
         }
         
-        bool operator!= (iterator other) const {
+        bool operator!= (const_iterator other) const {
             return !(other == *this);
         }
     };
-    
-public:
     
     HashMap(Hash hash_function = Hash()) : hasher(hash_function), cur_size(0), cur_capacity(min_cnt_rows) {
         table.resize(min_cnt_rows);
@@ -138,20 +149,36 @@ public:
     template<class Iterator>
     HashMap(Iterator begin, Iterator end, Hash hash_function = Hash()) : hasher(hash_function), cur_size(0), cur_capacity(min_cnt_rows) {
         table.resize(min_cnt_rows);
-        for (auto it = begin; it != end; ++it) {
-            size_t ind = hasher(it->first) % cur_capacity;
-            table[ind].push_back(*it);
-            ++cur_size;
+        for (auto it = begin; it != end; ++it) insert(*it);
+    }
+    
+    HashMap(std::initializer_list<std::pair<KeyType, ValueType>> list, Hash hash_function = Hash()) : hasher(hash_function), cur_size(0), cur_capacity(min_cnt_rows) {
+        table.resize(min_cnt_rows);
+        for (const auto& el : list) insert(el);
+    }
+    
+    HashMap(const HashMap& other) : HashMap(other.hasher) {
+        for (const auto& el : other) {
+            insert(el);
         }
     }
     
-    HashMap(std::initializer_list<node> list, Hash hash_function = Hash()) : hasher(hash_function), cur_size(0), cur_capacity(min_cnt_rows) {
+    HashMap operator= (const HashMap& other) {
+        hasher = other.hasher;
         table.resize(min_cnt_rows);
-        for (auto el : list) {
-            size_t ind = hasher(el.first) % cur_capacity;
-            table[ind].push_back(el);
-            ++cur_size;
+        cur_capacity = min_cnt_rows;
+        for (const auto& el : other) {
+            insert(el);
         }
+        return *this;
+    }
+    
+    HashMap operator= (HashMap&& other) {
+        hasher = std::move(other.hasher);
+        table = std::move(other.table);
+        cur_size = other.cur_size;
+        cur_capacity = other.cur_capacity;
+        return *this;
     }
     
     size_t size() const {
@@ -167,27 +194,27 @@ public:
     }
     
     iterator begin() {
-        return iterator(&this, 0, 0);
+        return iterator(this, 0, 0);
     }
     
     iterator end() {
-        return iterator(&this, table.size(), 0);
+        return iterator(this, table.size(), 0);
     }
     
     const_iterator begin() const {
-        return const_iterator(&this, 0, 0);
+        return const_iterator(this, 0, 0);
     }
     
     const_iterator end() const {
-        return const_iterator(&this, table.size(), 0);
+        return const_iterator(this, table.size(), 0);
     }
     
-    void insert(const node& element) {
+    void insert(const std::pair<KeyType, ValueType>& element) {
         size_t ind = hasher(element.first) % cur_capacity;
-        for (const auto& el : table[ind]) {
-            if (el == element) return;
+        for (size_t i = 0; i < table[ind].size(); ++i) {
+            if (table[ind][i]->first == element.first) return;
         }
-        table[ind].push_back(element);
+        table[ind].push_back(node(new std::pair<const KeyType, ValueType>(element)));
         ++cur_size;
         rebuild();
     }
@@ -195,8 +222,8 @@ public:
     void erase(const KeyType& key) {
         size_t ind = hasher(key) % cur_capacity;
         for (size_t i = 0; i < table[ind].size(); ++i) {
-            if (table[i].first == key) {
-                table[i].erase(table[i].begin() + i);
+            if (table[ind][i]->first == key) {
+                table[ind].erase(table[ind].begin() + i);
                 break;
             }
         }
@@ -206,7 +233,7 @@ public:
     iterator find(const KeyType& key) {
         size_t ind = hasher(key) % cur_capacity;
         for (size_t i = 0; i < table[ind].size(); ++i) {
-            if (table[i].first == key) return iterator(&this, ind, i);
+            if (table[ind][i]->first == key) return iterator(this, ind, i);
         }
         return end();
     }
@@ -214,20 +241,36 @@ public:
     const_iterator find(const KeyType& key) const {
         size_t ind = hasher(key) % cur_capacity;
         for (size_t i = 0; i < table[ind].size(); ++i) {
-            if (table[i].first == key) return const_iterator(&this, ind, i);
+            if (table[ind][i]->first == key) return const_iterator(this, ind, i);
         }
         return end();
     }
     
-    //11
+    ValueType& operator[] (const KeyType& key) {
+        auto it = find(key);
+        if (it == end()) insert({key, ValueType()});
+        it = find(key);
+        return it->second;
+    }
+    
+    ValueType& at(const KeyType& key) {
+        auto it = find(key);
+        if (it == end()) throw std::out_of_range("");
+        else return it->second;
+    }
+    
+    const ValueType& at(const KeyType& key) const {
+        auto it = find(key);
+        if (it == end()) throw std::out_of_range("");
+        else return it->second;
+    }
+    
+    void clear() {
+        for (size_t ind = 0; ind < cur_capacity; ++ind) {
+            while (!table[ind].empty()) table.pop_back();
+        }
+        rebuild();
+    }
     
 };
 
-using namespace std;
-
-int main() {
-    vector<pair<int, int>> a = {{1, 2}, {3, 4}};
-    HashMap<int, int> mp({{1, 2}, {3, 5}});
-    
-    return 0;
-}
